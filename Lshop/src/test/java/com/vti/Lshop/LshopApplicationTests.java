3b7b024f769fcb7ff815d8c9fe7010f6df31fd3b
dailoc1205 <pandablack1205@gmail.com>
2024-01-09 09:38:54 +0700

    [Add] LShop Project

---
+++ Lshop/src/test/java/com/vti/Lshop/LshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.Lshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
